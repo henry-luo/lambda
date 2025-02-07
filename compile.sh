4be5460580c007f11b40e8464196915438065676
Henry Luo <henry-luo@users.noreply.github.com>
2025-02-07 12:35:45 +0800

    clean up compiling warnings

--- compile.sh
+++ compile.sh
@@ -5,6 +5,6 @@ layout_html.c layout_style_tree.c render.c compute_style.c lib/string_buffer/str
 -I/opt/homebrew/opt/lexbor/include -L/opt/homebrew/opt/lexbor/lib -llexbor \
 -I/opt/homebrew/Cellar/freetype/2.13.3/include/freetype2 -L/opt/homebrew/Cellar/freetype/2.13.3/lib -lfreetype \
 -I/opt/homebrew/include/fontconfig -L/opt/homebrew/lib -lfontconfig \
--Werror=sign-compare -Werror=implicit-function-declaration -Werror=return-type \
--Werror=format -Werror=free-nonheap-object -Werror=shadow -Werror=array-bounds -Werror=implicit-function-declaration \
+-Werror=undef -Werror=no-common -Werror=uninitialized -Werror=sign-compare -Werror=implicit-function-declaration -Werror=implicit-int -Werror=return-type \
+-Werror=format -Werror=free-nonheap-object -Werror=shadow -Werror=array-bounds -Werror=null-dereference \
 -Werror=pointer-arith -Werror=pointer-compare -Werror=pointer-sign -Werror=pointer-to-int-cast -Werror=pointer-truncate
