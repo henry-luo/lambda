58658d4ba8e90b2446cf15e2c41440eafa33be62
Henry Luo <henry-luo@users.noreply.github.com>
2025-03-11 09:04:21 +0800

    added clipping on filled rect rendering

--- surface.c
+++ surface.c
@@ -41,12 +41,12 @@ void fill_surface_rect(ImageSurface* surface, Rect* rect, uint32_t color) {
     if (!surface) return;
     if (!rect) { r = (Rect){0, 0, surface->width, surface->height};  rect = &r; }
     printf("fill rect: x:%d, y:%d, wd:%d, hg:%d, color:%x\n", rect->x, rect->y, rect->width, rect->height, color);
-    for (int i = 0; i < rect->height; i++) {
-        if (rect->y + i < 0 || rect->y + i >= surface->height) continue;
-        uint8_t* row_pixels = (uint8_t*)surface->pixels + (rect->y + i) * surface->pitch;
-        if (0 <= rect->x && rect->x + rect->width <= surface->width) {
-            _fill_row(row_pixels, rect->x, rect->width, color);
-        }
+    int left = max(0, rect->x), right = min(surface->width, rect->x + rect->width);
+    int top = max(0, rect->y), bottom = min(surface->height, rect->y + rect->height);
+    if (left >= right || top >= bottom) return; // rect outside the surface
+    for (int i = top; i < bottom; i++) {
+        uint8_t* row_pixels = (uint8_t*)surface->pixels + i * surface->pitch; // updated to use 'i'
+        _fill_row(row_pixels, left, right - left, color);
     }
 }
 
@@ -54,24 +54,26 @@ void fill_surface_rect(ImageSurface* surface, Rect* rect, uint32_t color) {
 void blit_surface_scaled(ImageSurface* src, Rect* src_rect, ImageSurface* dst, Rect* dst_rect) {
     Rect rect;
     if (!src || !dst || !dst_rect) return;
-    if (!src_rect) {
+    if (!src_rect) { // use the entire source image
         rect = (Rect){0, 0, src->width, src->height};
         src_rect = &rect;
     }
     printf("blit surface: src(%d, %d, %d, %d) to dst(%d, %d, %d, %d)\n", 
-        src_rect->x, src_rect->y, src_rect->width, src_rect->height, dst_rect->x, dst_rect->y, dst_rect->width, dst_rect->height);
+        src_rect->x, src_rect->y, src_rect->width, src_rect->height, 
+        dst_rect->x, dst_rect->y, dst_rect->width, dst_rect->height);
     float x_ratio = (float)src_rect->width / dst_rect->width;
     float y_ratio = (float)src_rect->height / dst_rect->height;
-    for (int i = 0; i < dst_rect->height; i++) {
-        if (dst_rect->y + i < 0 || dst_rect->y + i >= dst->height) continue;
-        uint8_t* row_pixels = (uint8_t*)dst->pixels + (dst_rect->y + i) * dst->pitch;
-        for (int j = 0; j < dst_rect->width; j++) {
-            if (dst_rect->x + j < 0 || dst_rect->x + j >= dst->width) continue;
+    int left = max(0, dst_rect->x), right = min(dst->width, dst_rect->x + dst_rect->width);
+    int top = max(0, dst_rect->y), bottom = min(dst->height, dst_rect->y + dst_rect->height);
+    if (left >= right || top >= bottom) return; // dst_rect outside the dst surface
+    for (int i = top; i < bottom; i++) {
+        uint8_t* row_pixels = (uint8_t*)dst->pixels + i * dst->pitch;
+        for (int j = left; j < right; j++) {
             // todo: support different scale mode, like SDL_SCALEMODE_LINEAR
-            int src_x = src_rect->x + j * x_ratio;
-            int src_y = src_rect->y + i * y_ratio;
+            int src_x = src_rect->x + (j - dst_rect->x) * x_ratio;
+            int src_y = src_rect->y + (i - dst_rect->y) * y_ratio;
             uint8_t* src_pixel = (uint8_t*)src->pixels + (src_y * src->pitch) + (src_x * 4);
-            uint8_t* dst_pixel = (uint8_t*)row_pixels + (dst_rect->x + j) * 4;
+            uint8_t* dst_pixel = (uint8_t*)row_pixels + (j * 4);
             // hardcoded for ABGR to RGBA conversion
             dst_pixel[0] = src_pixel[3];  // dst alpha channel
             dst_pixel[1] = src_pixel[2];  // dst blue channel
