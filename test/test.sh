9057d41cce4159d7eac92e3d3493c2142ce21ac5
Henry Luo <henry-luo@users.noreply.github.com>
2025-03-27 10:35:50 +0800

    clean up test

--- test/test.sh
+++ test/test.sh
@@ -10,6 +10,6 @@ zig cc -o test_layout_flex.exe test_layout_flex.c \
 -lcriterion -I/opt/homebrew/Cellar/criterion/2.4.2_2/include -L/opt/homebrew/Cellar/criterion/2.4.2_2/lib \
 -I/usr/local/include /usr/local/lib/libzlog.a
 
-./test_strbuf.exe
-./test_strview.exe
-./test_layout_flex.exe
+./test_strbuf.exe --verbose
+./test_strview.exe --verbose
+./test_layout_flex.exe --verbose
