10d03fa0843337c9cd50f2656f96f9edc41d0cd0
Henry Luo <henry-luo@users.noreply.github.com>
2025-09-30 16:41:19 +0800

    clean up

--- test/test_radian.sh
+++
@@ -1,5 +0,0 @@
-clang -o test_layout_flex.exe test_layout_flex.c \
--lcriterion -I/opt/homebrew/Cellar/criterion/2.4.2_2/include -L/opt/homebrew/Cellar/criterion/2.4.2_2/lib \
--I/usr/local/include -fms-extensions
-
-./test_layout_flex.exe --verbose
