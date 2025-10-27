b38a0f75d13189ddbed19a324933593f41ca173f
Henry Luo <henry-luo@users.noreply.github.com>
2025-10-27 23:50:22 +0800

    enhancing lambda css

--- radiant/cmd_layout.cpp
+++ radiant/cmd_layout.cpp
@@ -375,6 +375,8 @@ DomElement* build_dom_tree_from_element(Element* elem, Pool* pool, DomElement* p
     }
 
     const char* class_value = extract_element_attribute(elem, "class", pool);
+    fprintf(stderr, "[HTML] Element <%s>: class attribute = %s\n",
+            tag_name, class_value ? class_value : "(null)");
     if (class_value) {
         // parse multiple classes separated by spaces
         char* class_copy = (char*)pool_alloc(pool, strlen(class_value) + 1);
@@ -385,6 +387,7 @@ DomElement* build_dom_tree_from_element(Element* elem, Pool* pool, DomElement* p
             char* token = strtok(class_copy, " \t\n");
             while (token) {
                 if (strlen(token) > 0) {
+                    fprintf(stderr, "[HTML] Adding class '%s' to <%s>\n", token, tag_name);
                     dom_element_add_class(dom_elem, token);
                 }
                 token = strtok(nullptr, " \t\n");
