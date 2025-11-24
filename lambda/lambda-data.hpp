cd48cb81fc5fb908ba119a57768afaad1c8056c4
git <git@perpetuatech.net>
2025-11-24 12:31:49 +0800

    clean up

--- lambda/lambda-data.hpp
+++ lambda/lambda-data.hpp
@@ -36,30 +36,28 @@ extern "C" {
 // Forward declarations for C++ types
 class SchemaValidator;
 
-// Avoid conflicts with C++ headers by undefining after including lambda.h
 #include "lambda.hpp"
 #undef max
 #undef min
 
 #include "name_pool.hpp"
 #include "shape_pool.hpp"
-// typedef struct NamePool NamePool;
 
-void *memcpy(void *dest, const void *src, size_t n);
-void *memset(void *s, int c, size_t n);
-int memcmp(const void *s1, const void *s2, size_t n);
-size_t strlen(const char *s);
-int strcmp(const char *s1, const char *s2);
-int strncmp(const char *s1, const char *s2, size_t n);
-int strcasecmp(const char *s1, const char *s2);
-int strncasecmp(const char *s1, const char *s2, size_t n);
-char *strchr(const char *s, int c);
-char *strcpy(char *dest, const char *src);
-char *strncpy(char *dest, const char *src, size_t n);
-char *strdup(const char *s);
-char *strstr(const char *target, const char *source);
-char *strrchr(const char *s, int c);
-char *strtok(char *str, const char *delim);
+// void *memcpy(void *dest, const void *src, size_t n);
+// void *memset(void *s, int c, size_t n);
+// int memcmp(const void *s1, const void *s2, size_t n);
+// size_t strlen(const char *s);
+// int strcmp(const char *s1, const char *s2);
+// int strncmp(const char *s1, const char *s2, size_t n);
+// int strcasecmp(const char *s1, const char *s2);
+// int strncasecmp(const char *s1, const char *s2, size_t n);
+// char *strchr(const char *s, int c);
+// char *strcpy(char *dest, const char *src);
+// char *strncpy(char *dest, const char *src, size_t n);
+// char *strdup(const char *s);
+// char *strstr(const char *target, const char *source);
+// char *strrchr(const char *s, int c);
+// char *strtok(char *str, const char *delim);
 
 #ifdef __cplusplus
 }
