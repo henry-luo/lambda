1c570cd84466cdc57d0811fe987c9c7f88eab28d
Henry Luo <henry-luo@users.noreply.github.com>
2026-01-06 21:43:49 +0800

    enhanced text-indent support

--- radiant/view_pool.cpp
+++ radiant/view_pool.cpp
@@ -185,6 +185,8 @@ BlockProp* alloc_block_prop(LayoutContext* lycon) {
     prop->box_sizing = CSS_VALUE_CONTENT_BOX;  // default to content-box
     prop->given_width = prop->given_height = -1;  // -1 for not specified
     prop->given_width_percent = prop->given_height_percent = NAN;  // NAN for not percentage
+    prop->text_indent = 0;  // default to 0
+    prop->text_indent_percent = NAN;  // NAN means not percentage (deferred resolution)
     return prop;
 }
 
