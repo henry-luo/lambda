6f9975d1376faf1d040d7604e24d3735cf22e3ab
git <git@perpetuatech.net>
2026-01-06 20:47:07 +0800

    implemented new tree-sitter based latex parser

---
+++ lambda/tree-sitter-latex-math/bindings/c/tree-sitter-latex-math.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_LATEX_MATH_H_
+#define TREE_SITTER_LATEX_MATH_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_latex_math(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_LATEX_MATH_H_
