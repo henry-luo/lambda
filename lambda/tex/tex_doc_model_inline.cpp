3be85a38ae2e1235641140961d1cc7c24d0aca29
git <git@perpetuatech.net>
2026-01-19 17:11:25 +0800

    split doc_model into more files

---
+++ lambda/tex/tex_doc_model_inline.cpp
@@ -0,0 +1,16 @@
+// tex_doc_model_inline.cpp - Placeholder for future extraction
+//
+// This file will contain:
+// - Text commands (\textbf, \textit, \emph, etc.)
+// - Font size commands (\tiny, \large, etc.)
+// - Symbol commands (ellipsis, dashes, logos, etc.)
+// - Diacritic handling
+//
+// Currently, all implementations remain in tex_document_model.cpp.
+// This file exists to prepare for future modularization.
+
+// Note: No includes needed - this is a stub file
+
+namespace tex {
+// All implementations are currently in tex_document_model.cpp
+} // namespace tex
